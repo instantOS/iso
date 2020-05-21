af36b3c1f4ae18ebc7f90fe4c3d4512a97054a63
paperbenni <paperbenni@gmail.com>
2020-05-21 20:00:36 +0200

    add 32 bit repo

--- build.sh
+++ build.sh
@@ -19,11 +19,18 @@ else
 fi
 
 cd
-
 cd instantlive
-echo "[instant]" >>pacman.conf
-echo "SigLevel = Optional TrustAll" >>pacman.conf
-echo "Server = http://instantos.surge.sh" >>pacman.conf
+
+# default is 64 bit repo
+if ! uname -m | grep -q '^i'; then
+    echo "[instant]" >>pacman.conf
+    echo "SigLevel = Optional TrustAll" >>pacman.conf
+    echo "Server = http://instantos.surge.sh" >>pacman.conf
+else
+    echo "[instant]" >>pacman.conf
+    echo "SigLevel = Optional TrustAll" >>pacman.conf
+    echo "Server = http://instantos32.surge.sh" >>pacman.conf
+fi
 
 cat ~/.cache/iso/livesession.sh >>airootfs/root/customize_airootfs.sh
 
