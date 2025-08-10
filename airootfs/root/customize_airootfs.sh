842abf983cc4c2a14a23f197f3bfdd8389a1fe47
paperbenni <paperbenni@gmail.com>
2025-08-10 15:17:41 +0200

    more refactoring

--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -6,7 +6,10 @@
 
 ## This script is run on the live iso itself, NOT the host
 
+set -eo pipefail
 echo "building instantOS installation ISO"
+
+# TODO: check if this is running in a chroot
 
 touch /opt/livebuilder
 
@@ -14,7 +17,7 @@ echo "adding user"
 useradd -m -s /bin/bash instantos
 echo "instantos:instantos" | chpasswd
 
-rgroup() {
+add_default_group() {
     if ! grep -q "$1" /etc/group; then
         groupadd "$1"
     fi
@@ -22,19 +25,41 @@ rgroup() {
     gpasswd -a "instantos" "$1"
 }
 
-rgroup "autologin"
-rgroup "video"
-rgroup "video"
-rgroup "wheel"
-rgroup "input"
+setup_lightdm() {
+    echo "preparing lightdm"
+    # enable greeter
+    sed -i 's/^\[Seat:\*\]/\[Seat:\*\]\ngreeter-session=lightdm-gtk-greeter/g' \
+        /etc/lightdm/lightdm.conf
+    # enable autologin
+    sed -i "s/^\[Seat:\*\]/[Seat:*]\nautologin-user=instantos/g" \
+        /etc/lightdm/lightdm.conf
 
-mkdir -p /etc/instantos
-curl -s https://raw.githubusercontent.com/instantOS/iso/main/version >/etc/instantos/liveversion
+    # start GUI session using the shell, for some reason enabling the service
+    # in livesession.sh doesn't work
+    echo "sleep 2 && systemctl start lightdm" >>/root/.zshrc
+    echo "[ -e /opt/lightstart ] || systemctl start lightdm & touch /opt/lightstart" >> \
+        /etc/zsh/zshrc
+}
 
-cd || exit 1
+setup_root() {
+    # allow sudo
+    sed -i 's/# %wheel/%wheel/g' /etc/sudoers
+    # clear sudo password
+    echo "root ALL=(ALL) NOPASSWD:ALL #instantosroot" >>/etc/sudoers
+    echo "" >>/etc/sudoers
+}
+
+add_default_group "autologin"
+add_default_group "video"
+add_default_group "video"
+add_default_group "wheel"
+add_default_group "input"
+
+cd
 mkdir tmparch
-cd tmparch || exit 1
+cd tmparch
 
+# TODO: are these needed?
 git clone --depth 1 https://github.com/instantOS/instantARCH
 git clone --depth 1 https://github.com/instantOS/instantOS
 git clone --depth 1 https://github.com/instantOS/iso
@@ -45,16 +70,8 @@ bash instantOS/rootinstall.sh
 [ -e /etc/lightdm ] || mkdir -p /etc/lightdm
 cat /usr/share/instantdotfiles/rootconfig/lightdm-gtk-greeter.conf >/etc/lightdm/lightdm-gtk-greeter.conf
 
-echo "preparing lightdm"
-# enable greeter
-sed -i 's/^\[Seat:\*\]/\[Seat:\*\]\ngreeter-session=lightdm-gtk-greeter/g' /etc/lightdm/lightdm.conf
-# enable autologin
-sed -i "s/^\[Seat:\*\]/[Seat:*]\nautologin-user=instantos/g" /etc/lightdm/lightdm.conf
-# allow sudo
-sed -i 's/# %wheel/%wheel/g' /etc/sudoers
-# clear sudo password
-echo "root ALL=(ALL) NOPASSWD:ALL #instantosroot" >>/etc/sudoers
-echo "" >>/etc/sudoers
+setup_lightdm
+setup_root
 
 echo 'GRUB_THEME="/usr/share/grub/themes/instantos/theme.txt"' >>/etc/default/grub
 
@@ -65,18 +82,14 @@ rm /opt/livebuilder
 systemctl enable systemd-timesyncd.service
 
 # disable lock screen password for live user
-echo 'export NOILOCKPASSWORD="true"' >> /etc/environment
+echo 'export NOILOCKPASSWORD="true"' >>/etc/environment
 
 echo "tzupdate &" >>/root/.zshrc
-
-# start GUI session, for some reason enabling the service in livesession.sh doesn't work
-echo "sleep 2 && systemctl start lightdm" >>/root/.zshrc
-echo "[ -e /opt/lightstart ] || systemctl start lightdm & touch /opt/lightstart" >>/etc/zsh/zshrc
 
 # install dev tools
 curl -s 'https://raw.githubusercontent.com/instantOS/instantTOOLS/main/netinstall.sh' | bash
 
-cd || exit 1
+cd
 rm -rf tmparch
 
 echo "finished building instantOS installation ISO"
